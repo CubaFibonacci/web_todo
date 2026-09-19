4953691da78d8fa72176eab18a726fc1f435bc3d
MuhammadSahrulRamadhan12 <sahrulramadhan08964@gmail.com>
2026-09-19 17:09:52 +0700

    penambahan local storage

--- todoapps/js/script.js
+++ todoapps/js/script.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         addTodo();
     });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
 });
 
 function addTodo() {
@@ -18,6 +22,7 @@ function addTodo() {
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function generateId() {
@@ -32,11 +37,6 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
         isCompleted
     }
 }
-
-document.addEventListener(RENDER_EVENT, function () {
-    console.log(todos);
-});
-
 
 function makeTodo(todoObject) {
     const textTitle = document.createElement('h2');
@@ -84,19 +84,6 @@ function makeTodo(todoObject) {
     return container;
 }
 
-
-document.addEventListener(RENDER_EVENT, function () {
-    const uncompletedTODOList = document.getElementById('todos');
-    uncompletedTODOList.innerHTML = '';
-
-    for (const todoItem of todos) {
-        const todoElement = makeTodo(todoItem);
-        if (!todoItem.isCompleted) {
-            uncompletedTODOList.append(todoElement);
-        }
-    }
-});
-
 function addTaskToCompleted(todoId) {
     const todoTarget = findTodo(todoId);
 
@@ -104,6 +91,7 @@ function addTaskToCompleted(todoId) {
 
     todoTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodo(id) {
@@ -139,8 +127,8 @@ function removeTaskFromCompleted(todoId) {
 
     todos.splice(todoTarget, 1);
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
-
 
 function undoTaskFromCompleted(todoId) {
     const todoTarget = findTodo(todoId);
@@ -149,6 +137,7 @@ function undoTaskFromCompleted(todoId) {
 
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodoIndex(todoId) {
@@ -159,4 +148,40 @@ function findTodoIndex(todoId) {
     }
 
     return -1;
+}
+
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+
+function isStorageExist() /* boolean */ {
+    if (typeof (Storage) === 'undefined') {
+        alert('Browser kamu tidak mendukung local storage');
+        return false;
+    }
+    return true;
+}
+
+function saveData() {
+    if (isStorageExist()) {
+        const parsed = JSON.stringify(todos);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const todo of data) {
+            todos.push(todo);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
 }
